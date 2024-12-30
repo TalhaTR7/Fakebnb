096d34356ac60dad738f8aa346ef8ffcc6315d75
TalhaTR7 <www.talharafat@gmail.com>
2024-12-30 16:52:00 +0500

    updated backend

---
+++ backend/routes/booking.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { createBooking, getBooking, updateBooking, deleteBooking } from '../controllers/bookingController';
+
+
+const router = express.Router();
+
+router.post('/', createBooking);
+router.get('/:id', getBooking);
+router.put('/:id', updateBooking);
+router.delete('/:id', deleteBooking);
+
+module.exports = router;
